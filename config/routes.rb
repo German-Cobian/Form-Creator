16b3b8546101540130021a1190be03ebb33b8977
German Renato Cobian <germancobian@hotmail.com>
2021-04-14 10:39:05 -0700

    Create a User model and its controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  root 'users#new'
+  resources :users, only: %i[new create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
