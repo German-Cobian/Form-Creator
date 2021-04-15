6a895804d79a202e9a99c1ff9271a487ff54fb32
German Renato Cobian <germancobian@hotmail.com>
2021-04-15 10:02:01 -0700

    Create a new user view using form_with

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(username: params[:username], email: params[:email], password: params[:password])
-    # @user = User.new(user_params)
+    #@user = User.new(user_params)
     if @user.save
       redirect_to new_user_path
     else
