82b0a53452037371a42bee91dc0391fcbc9cf4b4
Promise Johnson <chiemelapromise30@gmail.com>
2021-04-16 09:17:42 +1300

    The edit controller fixed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,15 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to edit_user_path
+    else
+      render :edit
+    end
+  end                          
+
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   root 'users#new'
-  resources :users, only: %i[new create]
-  get 'users/:id', to: 'users#edit'
   resources :users, only: [ :new, :create, :edit, :update ]
+  get 'users/:id', to: 'users#edit'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
