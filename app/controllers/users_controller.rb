7f9959f8485c1e99dbc47dc8b840d40da6fe25e7
Promise Johnson <chiemelapromise30@gmail.com>
2021-04-16 06:58:43 +1300

    Form_with rails helper form modified

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,13 +4,17 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(username: params[:username], email: params[:email], password: params[:password])
-    #@user = User.new(user_params)
+    # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    @user = User.new(user_params)
     if @user.save
       redirect_to new_user_path
     else
       render :new
     end
+  end
+
+  def edit
+    @user = User.find(params[:id])
   end
 
   def user_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'users#new'
   resources :users, only: %i[new create]
+  get 'users/:id', to: 'users#edit'
+  resources :users, only: [ :new, :create, :edit, :update ]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
