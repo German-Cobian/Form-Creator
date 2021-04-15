e1217b73ce5ddb4c940b7bbbf935e656f751ce68
German Renato Cobian <germancobian@hotmail.com>
2021-04-15 09:00:29 -0700

    Create a hash for the new users view and a private method for parameters at controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,19 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
+    #@user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
   end
 end
